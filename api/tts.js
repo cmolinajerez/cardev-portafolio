d302815791af32384e8638944b7617b4545c084a
Carla Molina <cmolinajerez@gmail.com>
2025-12-10 19:19:50 -0300

    Vuelta al modelo más narural ya que el rápido es horrible

--- api/tts.js
+++ api/tts.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
         },
         body: JSON.stringify({
           text: text,
-          model_id: 'eleven_turbo_v2', // Modelo en español
+          model_id: 'eleven_multilingual_v2', // Modelo en español
           voice_settings: {
             stability: 0.5,
             similarity_boost: 0.75,
